5dc2beee0a0bee9d09da415a9809bede74652db5
JuanFe0209 <piperbueno@hotmail.com>
2023-09-25 07:44:40 -0500

    .

---
+++ src/main/java/com/example/services/impl/LoginServiceSessionImpl.java
@@ -0,0 +1,19 @@
+package com.example.services.impl;
+
+import com.example.services.LoginService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Optional;
+
+public class LoginServiceSessionImpl implements LoginService {
+    @Override
+    public Optional<String> getUsername(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        if (username != null) {
+            return Optional.of(username);
+        }
+        return Optional.empty();
+    }
+}
